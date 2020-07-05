9f9ae8fe5a0c6fb109203519e33f572c937dd8ad
George Kirby <georgekirby@Georges-MacBook-Pro.local>
2020-07-05 08:05:23 +0100

    placeholder image; basic sections

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,98 +1,116 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <button v-on:click="showTips = !showTips">
-      {{ showTips ? "Hide Vue tips" : "Show Vue tips" }}
-    </button>
-    <div v-if="showTips" class="hello">
-      <p>
-        For a guide and recipes on how to configure / customize this project,<br />
-        check out the
-        <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-          >vue-cli documentation</a
-        >.
-      </p>
-      <h3>Installed CLI Plugins</h3>
-      <ul>
-        <li>
-          <a
-            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-            target="_blank"
-            rel="noopener"
-            >babel</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-            target="_blank"
-            rel="noopener"
-            >eslint</a
-          >
-        </li>
-      </ul>
-      <h3>Essential Links</h3>
-      <ul>
-        <li>
-          <a href="https://vuejs.org" target="_blank" rel="noopener"
-            >Core Docs</a
-          >
-        </li>
-        <li>
-          <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-            >Forum</a
-          >
-        </li>
-        <li>
-          <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-            >Community Chat</a
-          >
-        </li>
-        <li>
-          <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-            >Twitter</a
-          >
-        </li>
-        <li>
-          <a href="https://news.vuejs.org" target="_blank" rel="noopener"
-            >News</a
-          >
-        </li>
-      </ul>
-      <h3>Ecosystem</h3>
-      <ul>
-        <li>
-          <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-            >vue-router</a
-          >
-        </li>
-        <li>
-          <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
-            >vuex</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://github.com/vuejs/vue-devtools#vue-devtools"
-            target="_blank"
-            rel="noopener"
-            >vue-devtools</a
-          >
-        </li>
-        <li>
-          <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-            >vue-loader</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://github.com/vuejs/awesome-vue"
-            target="_blank"
-            rel="noopener"
-            >awesome-vue</a
-          >
-        </li>
-      </ul>
+    <div id="main">
+      <section class="hero">
+        <h1>George Kirby</h1>
+        <h3>Web designer & developer</h3>
+      </section>
+      <section class="intro"></section>
+      <section class="projects"></section>
+      <section class="experience"></section>
+      <section class="interests"></section>
+      <section class="themes"></section>
+      <section class="contact"></section>
+      <section class="feedback"></section>
+    </div>
+    <div id="vueTips">
+      <button v-on:click="showTips = !showTips">
+        {{ showTips ? "Hide Vue tips" : "Show Vue tips" }}
+      </button>
+      <div v-if="showTips" class="hello">
+        <p>
+          For a guide and recipes on how to configure / customize this
+          project,<br />
+          check out the
+          <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+            >vue-cli documentation</a
+          >.
+        </p>
+        <h3>Installed CLI Plugins</h3>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+              target="_blank"
+              rel="noopener"
+              >babel</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+              target="_blank"
+              rel="noopener"
+              >eslint</a
+            >
+          </li>
+        </ul>
+        <h3>Essential Links</h3>
+        <ul>
+          <li>
+            <a href="https://vuejs.org" target="_blank" rel="noopener"
+              >Core Docs</a
+            >
+          </li>
+          <li>
+            <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+              >Forum</a
+            >
+          </li>
+          <li>
+            <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+              >Community Chat</a
+            >
+          </li>
+          <li>
+            <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+              >Twitter</a
+            >
+          </li>
+          <li>
+            <a href="https://news.vuejs.org" target="_blank" rel="noopener"
+              >News</a
+            >
+          </li>
+        </ul>
+        <h3>Ecosystem</h3>
+        <ul>
+          <li>
+            <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+              >vue-router</a
+            >
+          </li>
+          <li>
+            <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
+              >vuex</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://github.com/vuejs/vue-devtools#vue-devtools"
+              target="_blank"
+              rel="noopener"
+              >vue-devtools</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://vue-loader.vuejs.org"
+              target="_blank"
+              rel="noopener"
+              >vue-loader</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://github.com/vuejs/awesome-vue"
+              target="_blank"
+              rel="noopener"
+              >awesome-vue</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -100,9 +118,6 @@
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
   data() {
     return {
       showTips: false,
@@ -113,9 +128,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
@@ -126,5 +138,16 @@ li {
 }
 a {
   color: #42b983;
+}
+
+section.hero {
+  min-height: 400px;
+  background-image: url("../../src/assets/freelance-stock-image.jpg");
+  background-size: cover;
+  background-position: center;
+}
+
+div#vueTips {
+  margin-top: 100px;
 }
 </style>
