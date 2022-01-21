ee7b7b28f4619c291c6ccfb2fc9d8a308b5d25ea
George Kirby <georgeskirby@gmail.com>
2022-01-21 15:23:37 +0000

    changes

--- src/content/Contact.js
+++ src/content/Contact.js
@@ -1,7 +1,8 @@
 const contact = {
-  mobile: "07XXX XXXXXX",
-  email: "x@gmail.com",
-  github: "github.com/x-x",
-};
-
-export { contact };
+    mobile: "07XXX XXXXXX",
+    email: "x@gmail.com",
+    github: "github.com/x-x",
+  };
+  
+  export { contact };
+  
